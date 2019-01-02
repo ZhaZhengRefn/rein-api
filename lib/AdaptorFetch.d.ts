1f9842abf9db6eb18598d846827ef8a7a6fa8b6d
zhazheng <512557403@qq.com>
2019-01-02 15:15:05 +0800

    feat: git ignore ./lib

--- lib/AdaptorFetch.d.ts
+++
@@ -1,9 +0,0 @@
-import { RequestConfig, AdaptorConfig, DefaultInit, FetchResponse, BaseAdaptor } from './types/adaptor-fetch';
-export default class AdaptorFetch extends BaseAdaptor {
-    private config;
-    private defaultInit;
-    private debug;
-    constructor(config?: AdaptorConfig, defaults?: DefaultInit);
-    private isSupportFetch;
-    apply(requestConfig: RequestConfig): Promise<FetchResponse>;
-}
--- lib/DispatchRequest.d.ts
+++
@@ -1,9 +0,0 @@
-import { BaseAdaptor } from './types/adaptor-fetch';
-import { DispatchConfig, DispatchOption } from './types/dispatch-request';
-export default class DispatchRequest {
-    private config;
-    private adaptor;
-    private debug;
-    constructor(config: DispatchConfig, adaptor: BaseAdaptor);
-    dispatch(options: DispatchOption): Promise<object>;
-}
